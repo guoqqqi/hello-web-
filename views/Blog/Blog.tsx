c690added1983cba5a00c5fdae7becf3bea53edd
guoqqqi <979918879@qq.com>
2020-10-21 19:34:12 +0800

    整理文档

---
+++ views/Blog/Blog.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import React from "react"
+
+type Props = {};
+
+const Blog: NextPage<Props, any> = () => {
+    return(
+        <div>welcome to my page !</div>
+    );
+};
+
+Blog.getInitialProps = async () => {
+    return{};
+};
+
+export default (Blog);
