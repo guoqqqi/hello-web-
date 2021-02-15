b6d808ca21f2b9ed157a3c8b3fa0afbb0ae84a86
guoqqqi <979918879@qq.com>
2021-02-15 18:46:32 +0800

    fix: update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fbe866ac129b4844ae8f";
+/******/ 	var hotCurrentHash = "c632373dfdc524c47723";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
