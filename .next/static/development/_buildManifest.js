ab2e4e6632602ac5c4913cc59735ae1e2098e21b
guoqqqi <979918879@qq.com>
2021-04-05 17:28:20 +0800

    feat: update codes

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/boke":["static\u002Fchunks\u002Fpages\u002Fboke.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fboke"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
