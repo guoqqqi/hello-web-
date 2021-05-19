1ab1304e1e3549b44c48f106e24a5f371e64eb15
guoqqqi <979918879@qq.com>
2021-05-19 23:41:41 +0800

    feat: update codes

--- views/Boke/Boke.tsx
+++ views/Boke/Boke.tsx
@@ -11,21 +11,28 @@ type Props = {};
 const Boke: NextPage<Props, any> = ({ }) => {
   const [value, setValue] = useState("苹果播客");
   const [showData, setShowData] = useState({});
+  const [fetchUrl, setFetchUrl] = useState('');
 
   useEffect(() => {
     const url = decodeURIComponent(location.href);
-    console.log(url);
     let obj = {};
-    if (url.indexOf('?') !== -1) {
+    if (url.indexOf('rss_url') === -1) {
       const urlsplit = url.split('?');
       let par = urlsplit[1].split('&');
       for (let i = 0; i < par.length; i++) {
         let p = par[i].split('=');
-        console.log(p[1]);
         obj[p[0]] = p[1];
         setShowData(obj);
       }
-    } else false;
+    } else {
+      const urlsplit = url.split('?');
+      let par = urlsplit[1].split('&');
+      let p = par[0].split('=');
+      setFetchUrl(p[1]);
+      console.log(p[1]);
+    };
+
+    // fetch fetchUrl
   }, []);
 
   return (
@@ -36,7 +43,7 @@ const Boke: NextPage<Props, any> = ({ }) => {
       </div>
       <SSection>
         <SBox>
-          <form id="form" action="" method="" >
+          <form id="form" action="" method="get" >
             <label htmlFor="" style={{ fontSize: '18px', paddingBottom: '10px' }}>从 RSS Feed 读取播客节目信息 </label>
             <SInner>
               <input placeholder="请输入 RSS Feed URL" type="text" name="rss_url" style={{ width: '400px', marginRight: '10px' }} />
